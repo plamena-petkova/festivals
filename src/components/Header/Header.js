2d9a06a19164c29900aa4079669142eb135acbb8
Plamena Petkova <pl.petkova@gmail.com>
2022-06-08 19:08:09 +0300

    react app initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,34 @@
+
+
+const Header = () => {
+    return (
+        <header>
+        <nav class="navigation">
+
+            <ul class="nav">
+                <div class="logo-wrapper">
+
+                    <li class="nav-links"><a href="" class="links">Home</a></li>
+                </div>
+                <div class="wrapper">
+
+                    <li class="nav-links"><a href="" class="links">Festivals</a></li>
+                    <li class="nav-links"><a href="" class="links">Add Festival</a></li>
+                    <li class="nav-links"><a href="" class="links">Login</a></li>
+                    <li class="nav-links"><a href="" class="links">Register</a></li>
+                    <li class="nav-links"><a href="" class="links">Logout</a></li>
+                    <li class="nav-links"><a href="" class="links"><i class="fa-solid fa-cart-shopping"></i></a></li>
+                </div>
+            </ul>
+
+            <article class="img-background">
+                <img src="https://cdn.pixabay.com/photo/2015/09/05/20/39/acoustic-925174_1280.jpg" alt="picture"/>
+            </article>
+
+
+        </nav>
+    </header>
+    )
+}
+
+export default Header;
